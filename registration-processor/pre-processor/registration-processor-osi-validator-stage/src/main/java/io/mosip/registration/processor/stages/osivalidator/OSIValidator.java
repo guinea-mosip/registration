8e94747847dc21483a6fb2453149f0898b930656
Ankit Vaishnav <ankitvaishnav.iitb1991@gmail.com>
2021-05-31 13:24:23 +0530

    Age related changes in OSI Validator

--- registration-processor/pre-processor/registration-processor-osi-validator-stage/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidator.java
+++ registration-processor/pre-processor/registration-processor-osi-validator-stage/src/main/java/io/mosip/registration/processor/stages/osivalidator/OSIValidator.java
@@ -108,6 +108,9 @@ public class OSIValidator {
 	@Value("${mosip.kernel.applicant.type.age.limit}")
 	private String ageLimit;
 
+	@Value("${mosip.kernel.applicant.type.minor.limit}")
+	private String minorAgeLimit;
+
 	@Value("${registration.processor.applicant.dob.format}")
 	private String dobFormat;
 
@@ -460,7 +463,7 @@ public class OSIValidator {
 		if (registrationStatusDto.getRegistrationType().equalsIgnoreCase(SyncTypeDto.NEW.name())
 				|| (registrationStatusDto.getRegistrationType().equalsIgnoreCase(SyncTypeDto.UPDATE.name()))) {
 			int age = utility.getApplicantAge(registrationId, registrationStatusDto.getRegistrationType(), ProviderStageName.OSI_VALIDATOR);
-			int ageThreshold = Integer.parseInt(ageLimit);
+			int ageThreshold = Integer.parseInt(minorAgeLimit);
 			if (age < ageThreshold) {
 				if (!introducerValidation)
 					return true;
